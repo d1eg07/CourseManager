bf4afe54d83a644bc0c7a7b39d3aea99080797a7
Diego <izayakazuto97@gmail.com>
2020-02-22 18:09:20 -0300

    Create CursosEmJson.php

---
+++ src/Controller/CursosEmJson.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Doctrine\ORM\EntityManagerInterface;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CursosEmJson implements RequestHandlerInterface
+{
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private $repositorioDeCursos;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->repositorioDeCursos = $entityManager
+            ->getRepository(Curso::class);
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $cursos = $this->repositorioDeCursos->findAll();
+        return new Response(
+            200,
+            ['Content-Type' => 'application/json'],
+            json_encode($cursos)
+        );
+    }
+}
