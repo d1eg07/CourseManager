24b26037ed80b806ca1d50e94c5f86180f4826af
Diego <izayakazuto97@gmail.com>
2020-02-22 18:11:02 -0300

    Create FormularioLogin.php

---
+++ src/Controller/FormularioLogin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Helper\RenderizadorDeHtmlTrait;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class FormularioLogin implements RequestHandlerInterface
+{
+    use RenderizadorDeHtmlTrait;
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $html = $this->renderizaHtml('login/formulario.php', [
+            'titulo' => 'Login'
+        ]);
+
+        return new Response(200, [], $html);
+    }
+}
