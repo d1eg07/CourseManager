e8b494d723cec13a4e71a5efa018680bb7dd42b2
Diego <izayakazuto97@gmail.com>
2020-02-22 18:12:31 -0300

    Create Usuario.php

---
+++ src/Entity/Usuario.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Cursos\Entity;
+
+/**
+ * @Entity
+ * @Table(name="usuarios")
+ */
+class Usuario
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+    /**
+     * @Column(type="string")
+     */
+    private $email;
+    /**
+     * @Column(type="string")
+     */
+    private $senha;
+
+    public function senhaEstaCorreta(string $senhaPura): bool
+    {
+        return password_verify($senhaPura, $this->senha);
+    }
+}
