00d00f0de49c0fac03c2ceab4bc65b344d7ace78
Diego <izayakazuto97@gmail.com>
2020-02-22 18:10:44 -0300

    Create FormularioInsercao.php

---
+++ src/Controller/FormularioInsercao.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Helper\RenderizadorDeHtmlTrait;
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class FormularioInsercao implements RequestHandlerInterface
+{
+    use RenderizadorDeHtmlTrait;
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $html = $this->renderizaHtml('cursos/formulario.php', [
+            'titulo' => 'Novo curso'
+        ]);
+        return new Response(200, [], $html);
+    }
+}
